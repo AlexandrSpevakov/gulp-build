d8c7ccbce6aa6a703d2be12c2b032decd6fc2a8b
Alexandr Spevakov <alexandr.pobeda99@gmail.com>
2022-02-03 01:12:57 +0200

    completed es6 pug.js

--- task/pug.js
+++ task/pug.js
@@ -7,5 +7,19 @@ import app from '../config/app.js';
 // Plugins
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
-import oug from 'gulp-pug';
+import pug from 'gulp-pug';
 import webpHtml from 'gulp-webp-html';
+
+// Processing Pug
+export default () => {
+   return gulp.src(path.pug.src)
+      .pipe(plumber({
+         errorHandler: notify.onError(error => ({
+            title: 'Pug',
+            massage: error.massage,
+         })),
+      }))
+      .pipe(pug(app.pug))
+      .pipe(webpHtml())
+      .pipe(gulp.dest(path.pug.dest));
+};
