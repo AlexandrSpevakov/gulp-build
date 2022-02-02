f26d628df2e7788a2f5620dadc941231dbe6e314
Alexandr Spevakov <alexandr.pobeda99@gmail.com>
2022-02-03 01:02:44 +0200

    added tasks files and pug.js plugins

---
+++ task/pug.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp';
+
+// Configurations
+import path from '../config/path.js';
+import app from '../config/app.js';
+
+// Plugins
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import oug from 'gulp-pug';
+import webpHtml from 'gulp-webp-html';
