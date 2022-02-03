639121664b01178f69cabd18d4956e17bfa0609c
Alexandr Spevakov <alexandr.pobeda99@gmail.com>
2022-02-03 15:13:57 +0200

    webp support

--- gulpfile.babel.js/task/scss.js
+++ gulpfile.babel.js/task/scss.js
@@ -12,6 +12,8 @@ import rename from 'gulp-rename';
 import size from 'gulp-size';
 import compilerSass from 'sass';
 import gulpSass from 'gulp-sass';
+import webpCss from 'gulp-webp-css';
+
 const sass = gulpSass(compilerSass);
 
 // Processing SCSS
@@ -24,6 +26,7 @@ export default () => {
          })),
       }))
       .pipe(sass())
+      .pipe(webpCss())
       .pipe(autoprefixer())
       .pipe(size({ title: 'main.css' }))
       .pipe(gulp.dest(path.scss.dest, { sourcemaps: true }))
